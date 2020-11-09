62985705d7030c58d382c969411f36572b07ceee
shanky0009 <shashank190897@gmail.com>
2020-11-10 00:17:43 +0530

    added basic tests to check pipeline

---
+++ tests/test_preprocessing.py
@@ -0,0 +1,34 @@
+import sys
+sys.path.append("../src")
+
+import preprocessing
+
+def test_that_get_height_works():
+    """
+    Test to check if we get correct height of depthmap.
+    """
+    # Setup - None
+    preprocessing.setHeight(int(180 * 0.75))
+    # Exercise
+    result = preprocessing.getHeight()
+
+    # Verify
+    truth = 135
+    assert result==truth
+
+    # Cleanup - none required
+
+def test_that_get_width_works():
+    """
+    Test to check if we get correct width of depthmap.
+    """
+    # Setup - None
+    preprocessing.setWidth(int(240 * 0.75))
+    # Exercise
+    result = preprocessing.getWidth()
+
+    # Verify
+    truth = 180
+    assert result==truth
+
+    # Cleanup - none required
