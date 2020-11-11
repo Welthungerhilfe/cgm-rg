5f8534c0713cb352b69006049024c55919a72a2d
shanky0009 <shashank190897@gmail.com>
2020-11-11 16:57:52 +0530

    fixed import error

--- tests/test_blur.py
+++ tests/test_blur.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 import sys
 
-sys.path.append(str(Path(__file__).parents[1]))
+# sys.path.append(str(Path(__file__).parents[1]))
+sys.path.append("../src")
 
-from blur import blur_faces_in_file  # noqa: E402
+from utils.preprocessing import blur_faces_in_file  # noqa: E402
 
 IMAGE_FNAME = "rgb_1583438117-71v1y4z0gd_1592711198959_101_74947.76209955901.jpg"
+
 
 def test_blur_faces_in_file():
     source_path = str(Path(__file__).parent / IMAGE_FNAME)
