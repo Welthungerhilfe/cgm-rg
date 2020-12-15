22c5432a7d1cd538849f4c68108ec2d91a703041
gangooteli <24511086+gangooteli@users.noreply.github.com>
2020-12-15 20:18:11 +0530

    Remove unnecessary code and renamed files

--- src/post_files_to_qa.py
+++ src/post_files_to_qa.py
@@ -1,8 +1,7 @@
 import requests
 
-headers = {}
-
-resource = "https%3A%2F%2Fcgmb2csandbox.onmicrosoft.com%2F98e9e1be-53fb-47f4-b53a-5842aeb869d5"
+resource = "https%3A%2F%2Fcgm-be-ci-qa-scanner-api.azurewebsites.net"
+url = 'https://cgm-be-ci-qa-scanner-api.azurewebsites.net'
 
 headers = {
     'Metadata': 'true',
@@ -12,6 +11,7 @@ response_one = requests.get(
     'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=' +
     resource,
     headers=headers)
+
 print("\nresponse_one status code: ", response_one.status_code)
 
 token = response_one.json()
@@ -23,7 +23,7 @@ print("\naccess_token: ", access_token)
 data = {"access_token": access_token}
 
 response_two = requests.post(
-    'https://cgm-be-ci-dev-scanner-api.azurewebsites.net/.auth/login/aad',
+    url + '/.auth/login/aad',
     json=data)
 print("\response_two status code: ", response_two.status_code)
 
@@ -38,7 +38,7 @@ headers = {
 }
 
 openapi_response = requests.get(
-    'https://cgm-be-ci-dev-scanner-api.azurewebsites.net/api/scan/openapi.json',
+    url + '/api/scan/openapi.json',
     headers=headers)
 print("\nOpenAPI response\n")
 print(openapi_response.json())
@@ -54,30 +54,18 @@ files['filename'] = path.split('/')[-1]
 print('\nFile name to post : ', files['filename'])
 
 headers['content_type'] = 'multipart/form-data'
-# headers['content-type'] = 'multipart/form-data'
-# headers['Content-Type'] = 'multipart/form-data'
 
 
-# url = "https://cgm-be-ci-dev-scanner-api.azurewebsites.net"
-# file_endpoint = '/api/scan/files'
-
-
-# content_type = 'multipart/form-data'
 
 post_file_response = requests.post(
-    'https://cgm-be-ci-dev-scanner-api.azurewebsites.net/api/scan/files',
+    url + '/api/scan/files',
     files=files,
     headers=headers)
-# post_file_response = requests.post('https://cgm-be-ci-dev-scanner-api.azurewebsites.net/api/scan/files', data=files, headers=headers)
-# post_file_response = requests.post('https://cgm-be-ci-dev-scanner-api.azurewebsites.net/api/scan/files', content_type=content_type, files=files, headers=headers)
 
 
-print(r"\post_file_response status code: ", post_file_response.status_code)
+print("\npost_file_response status code: ", post_file_response.status_code)
 print("\nPost File response\n")
 print(post_file_response.content)
-# print(post_file_response.json())
-
-# print("\nResponse status code: ", post_file_response.status_code)
 
 file_id = post_file_response.content.decode('utf-8')
 print("\nFile Id from post of test.jpg: ", file_id, '\n')
