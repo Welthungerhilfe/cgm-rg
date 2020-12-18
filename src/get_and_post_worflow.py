950275f3c388d97909c2d31b86a6c9db95495c9c
shanky0009 <shashank190897@gmail.com>
2020-12-18 10:41:04 +0530

    save response to db after successful post of workflow

--- src/get_and_post_worflow.py
+++ src/get_and_post_worflow.py
@@ -1,8 +1,30 @@
 import os
+import json
+import pymongo
+from pymongo import MongoClient
+
 from api_endpoints import ApiEndpoints
+
+def check_workflow_exists(workflow_path, collection):
+    with open(workflow_path) as f:
+        workflow_json = json.load(f)
+
+    results = collection.count_documents({"name": workflow_json["name"],"version": workflow_json["version"]})
+    print("no of existing document ", results)
+    return results
+
+
+def insert_workflow_document(workflow_path, collection):
+
+    with open(workflow_path) as f:
+        workflow_json = json.load(f)
+
+    collection.insert_one(workflow_json)
+
 
 if __name__ == "__main__":
 
+    os.environ["APP_ENV"] = "LOCAL"
     print("\nApp Environment : ", os.environ['APP_ENV'])
 
     if os.environ['APP_ENV'] == 'LOCAL':
@@ -26,12 +48,23 @@ if __name__ == "__main__":
         result_endpoint,
         workflow_endpoint)
 
-    blur_workflow_path = 'src/schema/blur-workflow.json'
-    blur_workflow_response_path = 'src/schema/blur-workflow-post.json'
+    cluster = client = pymongo.MongoClient("mongodb+srv://nikhil:bxQnvPpBDKI5VAnu@cluster0.y7zec.mongodb.net/<dbname>?retryWrites=true&w=majority")
+    db = cluster['cgm-rg']
+    collection = db['test']
 
-    status_code = cgm_api.post_workflow_and_save_response(
-        blur_workflow_path,
-        blur_workflow_response_path)
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    print(current_dir)
+    blur_workflow_path = os.path.join(current_dir, 'schema/blur-workflow.json')
+    blur_workflow_response_path = os.path.join(current_dir, 'schema/blur-workflow-post.json')
 
-    if status_code == 201:
-        print("Blur Workflow Registration Successful")
+    no_of_documents = check_workflow_exists(blur_workflow_path, collection)
+
+    if no_of_documents == 0:
+        status_code = cgm_api.post_workflow_and_save_response(
+            blur_workflow_path,
+            blur_workflow_response_path)
+
+        if status_code == 201:
+            print("Blur Workflow Registration Successful, adding to db")
+            insert_workflow_document(blur_workflow_response_path, collection)
+            
