9f0808747c27c58251e534f847a7e3f168ee5fa3
shanky0009 <shashank190897@gmail.com>
2020-12-24 09:45:10 +0530

    made changes to only post non existing workflows

--- src/get_and_post_worflow.py
+++ src/get_and_post_worflow.py
@@ -1,5 +1,33 @@
 import os
 from api_endpoints import ApiEndpoints
+import glob
+import json
+
+def get_list_of_files(source_folder):
+    '''
+    Get list of json files in a folder
+    '''
+    glob_search_path = os.path.join(source_folder, "*.json")
+    json_paths = glob.glob(glob_search_path)
+
+    return json_paths
+
+
+def check_workflow_exists(json_obj, workflows):
+    return any((d.get('name') == json_obj['name'] and d.get('version') == json_obj['version']) for d in workflows)
+
+
+def check_workflows(json_paths, workflows, cgm_api):
+    
+    for path in json_paths:
+        with open(path, 'r') as f:
+            workflow_obj = json.load(f)
+
+        if not check_workflow_exists(workflow_obj, workflows):
+            status_code = cgm_api.post_workflow_and_save_response(workflow_obj)
+            if status_code == 201:
+                print("successfully registered workflow for name ", workflow_obj['name'], " and version ", workflow_obj['version'])
+
 
 if __name__ == "__main__":
 
@@ -26,12 +54,12 @@ if __name__ == "__main__":
         result_endpoint,
         workflow_endpoint)
 
-    blur_workflow_path = 'src/schema/blur-workflow.json'
-    blur_workflow_response_path = 'src/schema/blur-workflow-post.json'
+    # blur_workflow_path = 'src/schema/blur-workflow.json'
+    # blur_workflow_response_path = 'src/schema/blur-workflow-post.json'
 
-    status_code = cgm_api.post_workflow_and_save_response(
-        blur_workflow_path,
-        blur_workflow_response_path)
+    workflow_paths = 'src/workflows'
+    json_paths = get_list_of_files(workflow_paths)
 
-    if status_code == 201:
-        print("Blur Workflow Registration Successful")
+    workflows = cgm_api.get_workflows()
+
+    check_workflows(json_paths, workflows['workflows'], cgm_api)
