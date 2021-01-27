a366376dc0207868555b239c81c2ae3ceaa0b766
shanky0009 <shashank190897@gmail.com>
2021-01-27 23:54:01 +0530

    added class to run weight flow

--- src/result_gen_with_api.py
+++ src/result_gen_with_api.py
@@ -234,7 +234,99 @@ class HeightFlow:
 
 
 class WeightFlow:
-    pass
+    def __init__(self, api, workflows, artifact_workflow_obj, scan_workflow_obj, artifacts, scan_parent_dir, scan_metadata):
+        self.api = api
+        self.workflows = workflows
+        self.artifacts = artifacts
+        self.artifact_workflow_obj = artifact_workflow_obj
+        self.scan_workflow_obj = scan_workflow_obj
+        self.scan_metadata = scan_metadata
+        self.scan_parent_dir = scan_parent_dir
+        if self.workflow_obj["data"]["input_format"] == 'application/zip':
+            self.depth_input_format = 'depth'
+        self.scan_directory = os.path.join(self.scan_parent_dir, self.scan_metadata['id'], self.depth_input_format)
+        self.artifact_workflow_obj['id'] = workflows.get_workflow_id(artifact_workflow_obj['name'], artifact_workflow_obj['version'])
+        self.scan_workflow_obj['id'] = workflows.get_workflow_id(scan_workflow_obj['name'], scan_workflow_obj['version'])
+
+    def bunch_object_to_json_object(self, bunch_object):
+        json_string = json.dumps(bunch_object, indent=2, separators=(',', ':'))
+        json_object = json.loads(json_string)
+
+        return json_object
+
+    def get_input_path(self, directory, file_name):
+        return os.path.join(directory, file_name)
+
+    def get_mean_scan_results(self, predictions):
+        return np.mean(predictions)
+
+    def process_depthmaps(self):
+        depthmaps = []
+        for artifact in self.artifacts:
+            input_path = self.get_input_path(
+                self.scan_directory,
+                artifact['file'])
+
+            data, width, height, depthScale, maxConfidence = preprocessing.load_depth(input_path)
+            depthmap, height, width = preprocessing.prepare_depthmap(data, width, height, depthScale)
+            depthmap = preprocessing.preprocess(depthmap)
+            depthmaps.append(depthmap)
+
+        depthmaps = np.array(depthmaps)
+
+        return depthmaps
+
+    def run_weight_flow(self):
+        depthmaps = self.process_depthmaps()
+        weight_predictions = inference.get_weight_predictions_local(depthmaps)
+        generated_timestamp = datetime.now().strftime('%Y-%m-%dT%H:%M:%SZ')
+        self.post_weight_results(weight_predictions, generated_timestamp)
+        
+    def artifact_level_weight_result_object(self, predictions, generated_timestamp):
+        res = Bunch()
+        res.results = []
+        for artifact, prediction in zip(self.artifacts, predictions):
+            weight_result = Bunch()
+            weight_result.id = f"{uuid.uuid4()}"
+            weight_result.scan = self.scan_metadata['id']
+            weight_result.workflow = self.artifact_workflow_obj["id"]
+            weight_result.source_artifacts = [artifact['id']]
+            weight_result.source_results = []
+            weight_result.generated = generated_timestamp
+            result = {'weight': prediction[0]}
+            weight_result.data = result
+            res.results.append(weight_result)
+
+        return res
+
+    def scan_level_weight_result_object(self, predictions, generated_timestamp):
+        res = Bunch()
+        res.results = []
+        weight_result = Bunch()
+        weight_result.id = f"{uuid.uuid4()}"
+        weight_result.scan = self.scan_metadata['id']
+        weight_result.workflow = self.scan_workflow_obj["id"]
+        weight_result.source_artifacts = [artifact['id'] for artifact in self.artifacts]
+        weight_result.source_results = []
+        weight_result.generated = generated_timestamp
+        mean_prediction = self.get_mean_scan_results(predictions)
+        result = {'mean_weight': mean_prediction}
+        weight_result.data = result
+
+        res.results.append(weight_result)
+
+        return res
+
+    def post_weight_results(self, predictions, generated_timestamp):
+        artifact_level_weight_result_bunch = self.artifact_level_weight_result_object(predictions, generated_timestamp)
+        artifact_level_weight_result_json = self.bunch_object_to_json_object(artifact_level_weight_result_bunch)
+        if self.api.post_results(artifact_level_weight_result_json) == 201:
+            print("successfully post artifact level weight results: ", artifact_level_weight_result_json)
+
+        scan_level_weight_result_bunch = self.artifact_level_weight_result_object(predictions, generated_timestamp)
+        scan_level_weight_result_json = self.bunch_object_to_json_object(scan_level_weight_result_bunch)
+        if self.api.post_results(scan_level_weight_result_json) == 201:
+            print("successfully post scan level weight results: ", scan_level_weight_result_json)
 
 
 class ProcessWorkflows:
