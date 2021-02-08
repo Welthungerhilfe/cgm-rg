4fb2dcb85550c2ca056b3dc8859cd14369e98c11
prajurock <prajwalsingh651@gmail.com>
2021-02-08 21:36:29 +0530

    download model from ws

---
+++ src/download_model.py
@@ -0,0 +1,25 @@
+import os
+from pathlib import Path
+
+from azureml.core import Workspace
+#from azureml.core.authentication import ServicePrincipalAuthentication
+from azureml.core.model import Model
+
+REPO_DIR = Path(__file__).parents[1].absolute()
+                                                                                    
+'''
+sp = ServicePrincipalAuthentication(
+        tenant_id=os.environ['TENANT_ID'],
+        service_principal_id=os.environ['SP_ID'],
+        service_principal_password=os.environ['SP_PASSWD'])
+
+ws = Workspace.get(name="cgm-azureml-prod",
+                    auth=sp,
+                    subscription_id=os.environ['SUB_ID'])
+'''
+ws = Workspace.from_config()
+
+standing_laying = Model(ws, name='standing_laying_classifier')
+print(standing_laying)
+standing_laying.download(
+    target_dir=REPO_DIR / 'models', exist_ok=True, exists_ok=None)
--- src/utils/inference.py
+++ src/utils/inference.py
@@ -22,9 +22,8 @@ os.environ["TF_CPP_MIN_LOG_LEVEL"] = "4"
 # pointclouds
 
 height_model = load_model('/app/models/best_model.h5')
-weight_model = load_model(
-    '/app/models/q4-depthmap-plaincnn-weight-95k-run-12/best_model.ckpt/', compile=False)
-standing_laying = load_model('/app/models/standing_laying.h5')
+weight_model = load_model('/app/models/q4-depthmap-plaincnn-weight-95k-run-12/best_model.ckpt/', compile=False)
+standing_laying = load_model('/app/models/Standing_laying/best_model.h5')
 
 
 def get_height_predictions_local(numpy_array):
