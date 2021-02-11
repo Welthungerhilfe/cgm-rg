b442e675c5bfa65303381e82a647129a109528da
prajurock <prajwalsingh651@gmail.com>
2021-02-11 23:22:17 +0530

    change in entry point

--- entrypoint_with_api.sh
+++ entrypoint_with_api.sh
@@ -3,7 +3,7 @@
 printenv | grep -v "no_proxy" >> /etc/environment
 
 python3 src/get_and_post_worflow.py
-python3 src/download_model.py
+#python3 src/download_model.py
 
 #python3 src/result_gen_with_api.py --url http://localhost:5001 --scan_parent_dir data/scans/ --blur_workflow_path src/schema/blur-workflow-post.json
 
