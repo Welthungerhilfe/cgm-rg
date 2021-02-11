bd8baaf9ef014044452d798896bc20df6be17ac5
prajurock <prajwalsingh651@gmail.com>
2021-02-11 14:14:19 +0530

    added resource group and workspace

--- src/download_model.py
+++ src/download_model.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-# import os
+import os
 
 from azureml.core import Workspace
 from azureml.core.authentication import ServicePrincipalAuthentication
@@ -7,15 +7,17 @@ from azureml.core.model import Model
 
 REPO_DIR = Path(__file__).parents[1].absolute()
 
-
 sp = ServicePrincipalAuthentication(
     tenant_id=os.environ['TENANT_ID'],
     service_principal_id=os.environ['SP_ID'],
     service_principal_password=os.environ['SP_PASSWD'])
 
-ws = Workspace.get(name="cgm-azureml-prod",
-                   auth=sp,
-                   subscription_id=os.environ['SUB_ID'])
+ws = Workspace(
+    subscription_id=os.environ['SUB_ID'],
+    resource_group="cgm-ml-prod-we-rg",
+    workspace_name="cgm-ml-prod-we-azml",
+    auth=sp
+)
 
 # ws = Workspace.from_config()
 
