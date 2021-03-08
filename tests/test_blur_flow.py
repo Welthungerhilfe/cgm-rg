b98fe6d5fe5973848fb6c02dd2eba298eb832e25
shanky0009 <shashank190897@gmail.com>
2021-03-08 12:21:10 +0530

    changed import based on new structure

--- tests/test_blur_flow.py
+++ tests/test_blur_flow.py
@@ -4,10 +4,13 @@ import pytest
 from bunch import Bunch
 import json
 import os
-# from src.result_gen_with_api import BlurFlow
-from result_gen_with_api import BlurFlow, ProcessWorkflows
+import pathlib
+from result_generation.blur import BlurFlow
+from result_gen_with_api import ProcessWorkflows
 # from api_endpoints import ApiEndpoints
 import set_up_dummy_objects
+
+current_working_directory = pathlib.Path.cwd()
 
 
 def test_bunch_object_to_json_object():
@@ -24,8 +27,6 @@ def test_bunch_object_to_json_object():
     truth = {'a': 1, 'b': 2}
     assert result == truth
     assert isinstance(truth, dict)
-
-    # Cleanup - none required
 
 
 def test_get_input_path():
@@ -44,5 +45,60 @@ def test_get_input_path():
     truth = 'app/scans/workflow.json'
     assert result == truth
 
-    # Cleanup - none required
 
+def test_blur_face_file_not_exists():
+    """
+    Test to check if face blur works if given path is not present
+    """
+    pass
+
+
+def test_blur_face_file_exists():
+    """
+    Test to check if face blur works if given path is present
+    """
+    # Setup
+    blurflow = set_up_dummy_objects.get_dummy_blur_flow_object()
+    input_file = str(current_working_directory.joinpath('tests', 'static_files', 'be1faf54-69c7-11eb-984b-a3ffd42e7b5a', 'img', 'bd8ba746-69c7-11eb-984b-23c55a7d518b'))
+
+    # Exercise
+    result = blurflow.blur_face(input_file)
+
+    # Verify
+    
+    assert result
+
+
+def test_post_files_successful():
+    """
+    Test to check if files are post to api successfully
+    """
+    pass
+
+
+def test_post_files_unsuccessful():
+    """
+    Test to check if files are not post to api
+    """
+    pass
+
+
+def test_prepare_result_object():
+    """
+    Test to check the result object
+    """
+    pass
+
+
+def test_post_result_object_successful():
+    """
+    Test to check successful upload of result object
+    """
+    pass
+
+
+def test_post_result_object_unsuccessful():
+    """
+    Test to check unsuccessful upload of result object
+    """
+    pass
