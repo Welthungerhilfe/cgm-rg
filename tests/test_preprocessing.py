ed1a6a271f3ee1cd5788352f21860f2b8dbe57e6
Markus Hinsche <m.hinsche@gmail.com>
2021-04-26 15:49:19 +0200

    Test with big and small dimension

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -5,15 +5,16 @@ import sys
 sys.path.append('./src')
 from utils import preprocessing
 
-current_working_directory = pathlib.Path.cwd()
-
+CWD = pathlib.Path.cwd()
+WIDTH = 240
+HEIGHT = 180
 
 def test_load_depth():
     """
     Test to check load depth function
     """
     # Setup
-    depth_file = current_working_directory.joinpath('tests', 'static_files', 'be1faf54-69c7-11eb-984b-a3ffd42e7b5a', 'depth', 'bd67cd9e-69c7-11eb-984b-77ac9d2b4986')
+    depth_file = CWD.joinpath('tests', 'static_files', 'be1faf54-69c7-11eb-984b-a3ffd42e7b5a', 'depth', 'bd67cd9e-69c7-11eb-984b-77ac9d2b4986')
 
     # Exercise
     data, width, height, depthScale, max_confidence = preprocessing.load_depth(depth_file)
@@ -26,9 +27,9 @@ def test_load_depth():
 
 def test_prepare_depthmap():
     # Setup
-    preprocessing.set_width(int(240 * 0.75))
-    preprocessing.set_height(int(180 * 0.75))
-    dummy_depth_data_file = current_working_directory.joinpath('tests', 'static_files', 'dummy_depth_data.pkl')
+    preprocessing.set_width(int(WIDTH * 0.75))
+    preprocessing.set_height(int(HEIGHT * 0.75))
+    dummy_depth_data_file = CWD.joinpath('tests', 'static_files', 'dummy_depth_data.pkl')
     with open(dummy_depth_data_file, 'rb') as f:
         dummy_depth_data = pickle.load(f)
 
@@ -40,17 +41,26 @@ def test_prepare_depthmap():
     assert result[0].shape[1] == result[1]
 
 
-def test_get_depthmaps():
-    FACTOR = 1 # 0.75
-    preprocessing.set_width(int(240 * FACTOR))
-    preprocessing.set_height(int(180 * FACTOR))
+def test_get_depthmaps_small():
+    path_small_depthmap = CWD.joinpath('tests', "4ed427b5-3fd9-4f4d-8e58-19e39c7d77b6")
+    depth_data, width, height, depth_scale, max_confidence = preprocessing.load_depth(path_small_depthmap)
+    assert (width, height) == (int(WIDTH * 0.75), int(HEIGHT * 0.75))
 
-    path_that_fails = current_working_directory.joinpath('tests', "4ed427b5-3fd9-4f4d-8e58-19e39c7d77b6") # 180x135
-    path_that_works = current_working_directory.joinpath('tests', "6b0c5a4f-d8f2-4658-b7f9-9406f1770259")
+    preprocessing.set_width(width)
+    preprocessing.set_height(height)
+    preprocessing.get_depthmaps([path_small_depthmap])
 
-    # assert preprocessing.get_depthmaps([path_that_fails])
-    preprocessing.load_depth(path_that_fails)
+
+def test_get_depthmaps_big():
+    path_big_depthmap = CWD.joinpath('tests', "6b0c5a4f-d8f2-4658-b7f9-9406f1770259")
+    depth_data, width, height, depth_scale, max_confidence = preprocessing.load_depth(path_big_depthmap)
+    assert (width, height) == (WIDTH, HEIGHT)
+
+    preprocessing.set_width(width)
+    preprocessing.set_height(height)
+    preprocessing.get_depthmaps([path_big_depthmap])
 
 
 if __name__ == "__main__":
-    test_get_depthmaps()
+    test_get_depthmaps_small()
+    test_get_depthmaps_big()
