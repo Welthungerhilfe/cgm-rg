4efaa6f7f357a300883e380947cc8fa47e986424
gangooteli <24511086+gangooteli@users.noreply.github.com>
2021-04-26 13:09:01 +0530

    Fixed RGB image orientation

--- src/result_generation/blur.py
+++ src/result_generation/blur.py
@@ -128,7 +128,7 @@ class BlurFlow:
         if self.scan_version in ["v0.1", "v0.2", "v0.4", "v0.5", "v0.6", "v0.7", "v0.8", "v0.9", "v1.0"]:
             # The images are provided in 90degrees turned. Here we rotate 90degress to
             # the right.
-            image = np.swapaxes(image, 0, 1)
+            image = cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)
             print("scan_version is ", self.scan_version)
             print("swapped image axis")
 
