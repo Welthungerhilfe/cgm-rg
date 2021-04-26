9b4e80591b280458f618849b0abb95ba92c733bf
Markus Hinsche <m.hinsche@gmail.com>
2021-04-26 16:51:39 +0200

    Refactor tests

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -9,10 +9,7 @@ CWD = pathlib.Path.cwd()
 WIDTH = 240
 HEIGHT = 180
 
-def test_load_depth():
-    """
-    Test to check load depth function
-    """
+def test_load_depth_hugh():
     # Setup
     depth_file = CWD.joinpath('tests', 'static_files', 'be1faf54-69c7-11eb-984b-a3ffd42e7b5a', 'depth', 'bd67cd9e-69c7-11eb-984b-77ac9d2b4986')
 
@@ -23,6 +20,20 @@ def test_load_depth():
     assert isinstance(data, bytes)
 
     # Cleanup - none required
+
+
+def test_load_depth_small():
+    path_small_depthmap = CWD.joinpath('tests', "4ed427b5-3fd9-4f4d-8e58-19e39c7d77b6")
+    depth_data, width, height, depth_scale, max_confidence = preprocessing.load_depth(path_small_depthmap)
+    assert (width, height) == (int(WIDTH * 0.75), int(HEIGHT * 0.75))
+
+    preprocessing.get_depthmaps([path_small_depthmap])
+
+
+def test_load_depth_big():
+    path_big_depthmap = CWD.joinpath('tests', "6b0c5a4f-d8f2-4658-b7f9-9406f1770259")
+    depth_data, width, height, depth_scale, max_confidence = preprocessing.load_depth(path_big_depthmap)
+    assert (width, height) == (WIDTH, HEIGHT)
 
 
 def test_prepare_depthmap():
@@ -41,25 +52,16 @@ def test_prepare_depthmap():
 
 def test_get_depthmaps_small():
     path_small_depthmap = CWD.joinpath('tests', "4ed427b5-3fd9-4f4d-8e58-19e39c7d77b6")
-    depth_data, width, height, depth_scale, max_confidence = preprocessing.load_depth(path_small_depthmap)
-    assert (width, height) == (int(WIDTH * 0.75), int(HEIGHT * 0.75))
-
-    preprocessing.set_width(width)
-    preprocessing.set_height(height)
     preprocessing.get_depthmaps([path_small_depthmap])
 
 
 def test_get_depthmaps_big():
     path_big_depthmap = CWD.joinpath('tests', "6b0c5a4f-d8f2-4658-b7f9-9406f1770259")
-    depth_data, width, height, depth_scale, max_confidence = preprocessing.load_depth(path_big_depthmap)
-    assert (width, height) == (WIDTH, HEIGHT)
-
-    preprocessing.set_width(width)
-    preprocessing.set_height(height)
     preprocessing.get_depthmaps([path_big_depthmap])
 
 
 if __name__ == "__main__":
+    test_load_depth_hugh()
     # test_get_depthmaps_small()
     # test_get_depthmaps_big()
-    test_prepare_depthmap()
+    # test_prepare_depthmap()
