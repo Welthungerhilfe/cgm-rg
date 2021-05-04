a1da05e29582230a22e9c3807a2df09b27f96074
gangooteli <24511086+gangooteli@users.noreply.github.com>
2021-05-05 00:00:08 +0530

    Fixed flake8 issue

--- src/api_endpoints.py
+++ src/api_endpoints.py
@@ -172,8 +172,8 @@ class ApiEndpoints:
         '''Get the scan metadata'''
         headers = self.prepare_header()
         # TODO use scan_version and workflow id
-        mod_scan_endpoint = self.mod_scan_endpoint + '&scan_version='+ scan_version
-        #mod_scan_endpoint = self.mod_scan_endpoint + '&scan_version='+ scan_version + '&workflow=' + workflow_id
+        mod_scan_endpoint = self.mod_scan_endpoint + '&scan_version=' + scan_version
+        # mod_scan_endpoint = self.mod_scan_endpoint + '&scan_version='+ scan_version + '&workflow=' + workflow_id
         print("modified scan endpoint : ", mod_scan_endpoint)
 
         response = requests.get(
