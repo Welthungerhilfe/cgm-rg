d2758a78aa77f9c513641d7cce83c85f1404d25c
prajurock <prajwalsingh651@gmail.com>
2021-05-06 00:31:28 +0530

    model_download for ensemble

--- src/download_model.py
+++ src/download_model.py
@@ -71,6 +71,25 @@ def main():
                    input_location=os.path.join('outputs', 'best_model.ckpt'),
                    output_location=REPO_DIR / 'models/depthmapmultiartifactlatefusion')
 
+    RUN_IDS=['q1-ensemble-warmup_1610544610_eb44bfe2', 'q1-ensemble-warmup_1610547587_7ca932c3',
+             'q1-ensemble-warmup_1610547669_5b789bd1', 'q1-ensemble-warmup_1610547705_f2141d0f',
+             'q1-ensemble-warmup_1610547744_d2b42ce5', 'q1-ensemble-warmup_1610547780_2f000a25',
+             'q1-ensemble-warmup_1610547816_c3f815df', 'q1-ensemble-warmup_1610547892_8ee6ff49',
+             'q1-ensemble-warmup_1610547928_b9519b6a', 'q1-ensemble-warmup_1610547986_ad0186b8',
+             'q1-ensemble-warmup_1610548023_99ac6060', 'q1-ensemble-warmup_1610548064_afefd4e4',
+             'q1-ensemble-warmup_1610548106_69993d24', 'q1-ensemble-warmup_1610548137_a8c52d63',
+             'q1-ensemble-warmup_1610548168_914ce1f6', 'q1-ensemble-warmup_1610548209_9692a253']
+    
+    for id in RUN_IDS:
+        print(f"Downloading run {id}")
+        download_model(
+            ws=ws,
+            experiment_name='q1-ensemble-warmup',
+            run_id=id,
+            input_location=os.path.join('outputs', 'best_model.ckpt'),
+            output_location=REPO_DIR / 'models/deepensemble'/id
+        )
+
 
 if __name__ == "__main__":
     main()
