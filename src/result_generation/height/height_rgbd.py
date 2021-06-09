bd593161c5d8a4de8c33a74ca5f840b9606484c5
Prashant <pksingh@childgrowthmonitor.com>
2021-06-09 06:11:58 +0000

    Fixing flake8

--- src/result_generation/height/height_rgbd.py
+++ src/result_generation/height/height_rgbd.py
@@ -1,4 +1,5 @@
 import sys
+import logging
 from datetime import datetime
 from pathlib import Path
 import cv2
@@ -13,7 +14,7 @@ sys.path.append(str(Path(__file__).parents[1]))
 import utils.inference as inference  # noqa: E402
 import utils.preprocessing as preprocessing  # noqa: E402
 
-CLOSEST_IMAGE_ORDER = 3
+ORDER_DIFFERENCE_ALLOWED = 3
 
 
 class HeightFlowRGBD(HeightFlow):
@@ -29,37 +30,39 @@ class HeightFlowRGBD(HeightFlow):
             self.result_generation.scan_parent_dir,
             self.result_generation.scan_metadata['id'],
             'img')
+
         image_order_ids = []
-        for image_artifacts in self.image_artifacts:
-            image_order_ids.append(image_artifacts['order']
+        for image_artifact in self.image_artifacts:
+            image_order_ids.append(image_artifact['order'])
 
         for artifact in self.artifacts:
-            input_path=self.result_generation.get_input_path(
+            input_path = self.result_generation.get_input_path(
                 self.scan_directory, artifact['file'])
-            depth_order=artifact['order']
-            closest_order_id=preprocessing.find_corresponding_image(image_order_ids, depth_order)
-
-            # closest_order_id = min(image_order_ids, key=lambda order:abs(order-img_id))
-            result_image_dict=next(
+            depth_id = artifact['order']
+            closest_order_id = preprocessing.find_corresponding_image(image_order_ids, depth_id)
+            if abs(closest_order_id - depth_id) >= ORDER_DIFFERENCE_ALLOWED:
+                logging.info("No corresponding image artifact found for the depthmap")
+                continue
+            result_image_dict = next(
                 iter(item for item in self.image_artifacts if item['order'] == closest_order_id), None)
             if result_image_dict:
-                image_input_path=self.result_generation.get_input_path(
+                image_input_path = self.result_generation.get_input_path(
                     scan_image_directory, result_image_dict['file'])
             else:
-                print("No RGB found for order:", depth_order)
+                logging.info("No RGB found for order:", depth_id)
                 continue
-            raw_image=cv2.imread(str(image_input_path))  # cv2.imread gives error when reading from posix path
-            rot_image=cv2.rotate(raw_image, cv2.ROTATE_90_CLOCKWISE)
-            image=preprocessing.preprocess_image(rot_image)
-            data, width, height, depth_scale, _max_confidence=preprocessing.load_depth(
+            raw_image = cv2.imread(str(image_input_path))  # cv2.imread gives error when reading from posix path
+            rot_image = cv2.rotate(raw_image, cv2.ROTATE_90_CLOCKWISE)
+            image = preprocessing.preprocess_image(rot_image)
+            data, width, height, depth_scale, _max_confidence = preprocessing.load_depth(
                 input_path)
-            depthmap=preprocessing.prepare_depthmap(
+            depthmap = preprocessing.prepare_depthmap(
                 data, width, height, depth_scale)
-            depthmap=preprocessing.preprocess(depthmap)
-            rgbd_data=tf.concat([image, depthmap], axis=2)
-            rgbd_data=tf.image.resize(
+            depthmap = preprocessing.preprocess(depthmap)
+            rgbd_data = tf.concat([image, depthmap], axis=2)
+            rgbd_data = tf.image.resize(
                 rgbd_data, (preprocessing.IMAGE_TARGET_HEIGHT,
                             preprocessing.IMAGE_TARGET_WIDTH))
             rgbd_scan.append(rgbd_data)
-        rgbd_scan=np.array(rgbd_scan)
+        rgbd_scan = np.array(rgbd_scan)
         return rgbd_scan
