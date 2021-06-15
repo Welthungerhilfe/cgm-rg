5a1cd2e327f8d3fb1449dc9ac9807d8ac58a9c7d
gangooteli <24511086+gangooteli@users.noreply.github.com>
2021-06-15 18:02:05 +0530

    Switching off cron job process

--- entrypoint_with_api.sh
+++ entrypoint_with_api.sh
@@ -7,4 +7,4 @@ python3 src/get_and_post_workflow.py
 
 #python3 src/result_gen_with_api.py --url http://localhost:5001 --scan_parent_dir data/scans/ --blur_workflow_path src/schema/blur-workflow-post.json
 
-cron -f
+#cron -f
