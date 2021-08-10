23850228e8a1751707ca2d5b2c8c64b61b2c4377
Markus Hinsche <m.hinsche@gmail.com>
2021-08-10 11:25:51 +0200

    Refactor height RGBD

--- src/result_generation/height/height_rgbd.py
+++ src/result_generation/height/height_rgbd.py
@@ -1,18 +1,19 @@
-import sys
-# import logging
 from datetime import datetime
-from pathlib import Path
-import cv2
-import tensorflow as tf
 import os
+from pathlib import Path
+import sys
 
+import cv2
 import numpy as np
+import tensorflow as tf
 
 from result_generation.height.height import HeightFlow
 
 sys.path.append(str(Path(__file__).parents[1]))
 import utils.inference as inference  # noqa: E402
-import utils.preprocessing as preprocessing  # noqa: E402
+from utils.preprocessing import (  # noqa: E402
+    find_corresponding_image, load_depth, preprocess, prepare_depthmap, preprocess_image,
+    IMAGE_TARGET_HEIGHT, IMAGE_TARGET_WIDTH)
 import log
 
 
@@ -33,39 +34,33 @@ class HeightFlowRGBD(HeightFlow):
             self.result_generation.scan_parent_dir,
             self.result_generation.scan_metadata['id'],
             'img')
-
-        image_order_ids = []
-        for image_artifact in self.image_artifacts:
-            image_order_ids.append(image_artifact['order'])
+        image_order_ids = [image_artifact['order'] for image_artifact in self.image_artifacts]
 
         for artifact in self.artifacts:
-            input_path = self.result_generation.get_input_path(
-                self.scan_directory, artifact['file'])
+            input_path = self.result_generation.get_input_path(self.scan_directory, artifact['file'])
             depth_id = artifact['order']
-            closest_order_id = preprocessing.find_corresponding_image(image_order_ids, depth_id)
+            closest_order_id = find_corresponding_image(image_order_ids, depth_id)
             if abs(closest_order_id - depth_id) >= ORDER_DIFFERENCE_ALLOWED:
                 logger.info("No corresponding image artifact found for the depthmap")
                 continue
-            result_image_dict = next(
-                iter(item for item in self.image_artifacts if item['order'] == closest_order_id), None)
+
+            gen = (item for item in self.image_artifacts if item['order'] == closest_order_id)
+            result_image_dict = next(iter(gen), None)
+
             if result_image_dict:
-                image_input_path = self.result_generation.get_input_path(
-                    scan_image_directory, result_image_dict['file'])
+                image_input_path = self.result_generation.get_input_path(scan_image_directory, result_image_dict['file'])
             else:
                 logger.info("%s %s", "No RGB found for order:", depth_id)
                 continue
+
             raw_image = cv2.imread(str(image_input_path))  # cv2.imread gives error when reading from posix path
             rot_image = cv2.rotate(raw_image, cv2.ROTATE_90_CLOCKWISE)
-            image = preprocessing.preprocess_image(rot_image)
-            data, width, height, depth_scale, _max_confidence = preprocessing.load_depth(
-                input_path)
-            depthmap = preprocessing.prepare_depthmap(
-                data, width, height, depth_scale)
-            depthmap = preprocessing.preprocess(depthmap)
+            image = preprocess_image(rot_image)
+            data, width, height, depth_scale, _max_confidence = load_depth(input_path)
+            depthmap = prepare_depthmap(data, width, height, depth_scale)
+            depthmap = preprocess(depthmap)
             rgbd_data = tf.concat([image, depthmap], axis=2)
-            rgbd_data = tf.image.resize(
-                rgbd_data, (preprocessing.IMAGE_TARGET_HEIGHT,
-                            preprocessing.IMAGE_TARGET_WIDTH))
+            rgbd_data = tf.image.resize(rgbd_data, (IMAGE_TARGET_HEIGHT, IMAGE_TARGET_WIDTH))
             rgbd_scan.append(rgbd_data)
         rgbd_scan = np.array(rgbd_scan)
         return rgbd_scan
