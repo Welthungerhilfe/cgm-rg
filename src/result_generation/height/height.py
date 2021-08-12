edbdcb175c2b0bdd45bc797121bcbbd4eeaeab39
gangooteli <24511086+gangooteli@users.noreply.github.com>
2021-08-12 13:39:16 +0530

    Added code to get the post the results seperately

--- src/result_generation/height/height.py
+++ src/result_generation/height/height.py
@@ -27,16 +27,18 @@ class HeightFlow:
             image_artifacts=None):
         store_attr('result_generation,artifact_workflow_path,scan_workflow_path,artifacts,person_details', self)
         self.image_artifacts = [] if image_artifacts is None else image_artifacts
+
         self.artifact_workflow_obj = self.result_generation.workflows.load_workflows(
             self.artifact_workflow_path)
-        self.scan_workflow_obj = self.result_generation.workflows.load_workflows(
-            self.scan_workflow_path)
         if self.artifact_workflow_obj["data"]["input_format"] == 'application/zip':
             self.depth_input_format = 'depth'
             self.scan_directory = Path(self.result_generation.scan_parent_dir) / \
                 self.result_generation.scan_metadata['id'] / self.depth_input_format
         self.artifact_workflow_obj['id'] = self.result_generation.workflows.get_workflow_id(
             self.artifact_workflow_obj['name'], self.artifact_workflow_obj['version'])
+
+        self.scan_workflow_obj = self.result_generation.workflows.load_workflows(
+            self.scan_workflow_path)
         self.scan_workflow_obj['id'] = self.result_generation.workflows.get_workflow_id(
             self.scan_workflow_obj['name'], self.scan_workflow_obj['version'])
 
@@ -92,12 +94,19 @@ class HeightFlow:
 
     def post_height_results(self, predictions, generated_timestamp):
         """Post the artifact and scan level height results to the API"""
+        self.post_artifact_level_height_results(predictions, generated_timestamp)
+        self.post_scan_level_height_results(predictions, generated_timestamp)
+
+    def post_artifact_level_height_results(self, predictions, generated_timestamp):
+        """Post scan level height results to the API"""
         artifact_level_height_result_bunch = self.artifact_level_result(predictions, generated_timestamp)
         artifact_level_height_result_json = self.result_generation.bunch_object_to_json_object(
             artifact_level_height_result_bunch)
         if self.result_generation.api.post_results(artifact_level_height_result_json) == 201:
             logger.info("%s %s", "successfully post artifact level height results:", artifact_level_height_result_json)
 
+    def post_scan_level_height_results(self, predictions, generated_timestamp):
+        """Post scan level height results to the API"""
         scan_level_height_result_bunch = self.scan_level_height_result_object(
             predictions, generated_timestamp, self.scan_workflow_obj)
         scan_level_height_result_json = self.result_generation.bunch_object_to_json_object(
@@ -142,3 +151,13 @@ class HeightFlow:
         result.data = result
         res.results.append(result)
         return res
+
+    def get_height_prediction_using_api(self):
+        height_results = self.result_generation.api.get_results(
+            self.result_generation.scan_metadata['id'], 
+            self.artifact_workflow_obj["id"]
+            )
+        # TODO add parser to get height_predictions
+        height_predictions = height_results
+        return height_predictions
+
