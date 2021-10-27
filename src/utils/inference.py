7dde8b9710bd7214c21854c8f383654246b8f284
Markus Hinsche <m.hinsche@gmail.com>
2021-10-27 19:54:28 +0200

    Remove unused code

--- src/utils/inference.py
+++ src/utils/inference.py
@@ -13,9 +13,6 @@ sys.path.append(
             os.path.realpath(__file__)),
         os.pardir))
 
-current_working_directory = Path.cwd()
-models_path = current_working_directory.joinpath('models')
-
 REPO_DIR = Path(os.environ['PWD']).absolute()
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "4"
 
