b4dbb55dfd12ac9d61e89563807454a296a5f697
Markus Hinsche <m.hinsche@gmail.com>
2021-10-27 20:02:29 +0200

    Simplify commands

--- entrypoint_with_api.sh
+++ entrypoint_with_api.sh
@@ -7,6 +7,6 @@ printenv | grep -v "no_proxy" >> /etc/environment
 python3 src/download_model.py
 python3 src/get_and_post_workflow.py
 
-#python3 src/result_gen_with_api.py --url http://localhost:5001 --scan_parent_dir data/scans/ --blur_workflow_path src/schema/blur-workflow-post.json
+#python3 src/result_gen_with_api.py
 
 cron -f
