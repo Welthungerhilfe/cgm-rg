817f537e1617df5cb44e6c3f390956c93c849ab3
prashant <prashant905@gmail.com>
2023-04-27 12:06:29 +0000

    Fixed utils.py flake8 error

--- src/result_generation/utils.py
+++ src/result_generation/utils.py
@@ -14,13 +14,6 @@ def calculate_age(dob, scan_date):
     return delta.days
 
 
-def is_child_standing_age_lt_2(age_in_days, scan_type):
-    child_standing_age_lt_2 = False
-    if int(age_in_days) < AGE_LT_2 and scan_type in STANDING_SCAN_STEPS:
-        child_standing_age_lt_2 = True
-    return child_standing_age_lt_2
-
-
 def remove_outliers(arr):
     arr = arr.flatten().tolist()
     q1 = np.percentile(arr, 25)
@@ -31,3 +24,10 @@ def remove_outliers(arr):
     result = [nums for nums in arr if lower <= nums <= upper]
     arr_new = np.array(result).reshape(len(result), 1)
     return arr_new
+
+
+def is_child_standing_age_lt_2(age_in_days, scan_type):
+    child_standing_age_lt_2 = False
+    if int(age_in_days) < AGE_LT_2 and scan_type in STANDING_SCAN_STEPS:
+        child_standing_age_lt_2 = True
+    return child_standing_age_lt_2
