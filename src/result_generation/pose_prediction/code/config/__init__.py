6c707a637fe43378003a767278be46000f661365
shanky0009 <shashank190897@gmail.com>
2023-07-12 08:59:12 +0000

    removed old code

--- src/result_generation/pose_prediction/code/config/__init__.py
+++
@@ -1,6 +0,0 @@
-import sys
-from pathlib import Path
-sys.path.append(str(Path(__file__).parents[1]))  # noqa: E402
-from result_generation.pose_prediction.code.config.default import _C as cfg  # noqa
-from result_generation.pose_prediction.code.config.default import update_config  # noqa
-from result_generation.pose_prediction.code.config.models import MODEL_EXTRAS  # noqa
