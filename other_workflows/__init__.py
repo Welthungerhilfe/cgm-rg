5df5914aef90e9ee4fa6de2157f691fdd0454a5c
shanky0009 <42682679+shanky0009@users.noreply.github.com>
2023-10-27 14:49:44 +0530

    added code to calculate depth feature angle (#210)

--- other_workflows/__init__.py
+++ other_workflows/__init__.py
@@ -11,6 +11,7 @@ from utils.standing_laying import sl_flow
 from utils.depth_img import depth_img_flow
 from utils.app_pose_visualization import run_app_pose_visualization_flow
 from utils.efficient_pose import run_efficient_pose_flow
+from utils.depth_features import run_depth_features_flow
 
 
 rgb_format = ["rgb", "image/jpeg"]
@@ -49,5 +50,6 @@ def main(msg: func.QueueMessage) -> None:
     run_pose_flow(cgm_api, scan_id, rgb_artifacts, workflows, scan_type, version, results)
     depth_img_flow(cgm_api, scan_id, depth_artifacts, workflows, results)
     run_efficient_pose_flow(cgm_api, scan_id, rgb_artifacts, workflows, scan_type, version, results)
+    run_depth_features_flow(cgm_api, scan_id, depth_artifacts, workflows, results)
     run_app_pose_visualization_flow(cgm_api, scan_id, rgb_artifacts, workflows, scan_type, version, results)
     # sl_flow(cgm_api, scan_id, rgb_artifacts, workflows)
